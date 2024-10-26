e876f2e6dada51385bf84166f8aa78558846e5df
Jin-Mach <Ji82Ma@seznam.cz>
2024-10-26 20:22:24 +0200

    GUI setup: colors, icons, fonts, combo boxes, tooltips, and QSS.

--- src/ui/widgets/file_toolbar.py
+++ src/ui/widgets/file_toolbar.py
@@ -1,8 +1,15 @@
-from PyQt6.QtCore import Qt
+import json
+import pathlib
+
+from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QToolBar, QWidget, QPushButton, QHBoxLayout, QLabel, QLineEdit
 
 
 class FileToolbar(QToolBar):
+    tooltips_path = str(pathlib.Path(__file__).parent.parent.joinpath("tooltips", "tooltips_en.json"))
+    icons_path = pathlib.Path(__file__).parent.parent.joinpath("icons", "file_icons")
+
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.setObjectName("fileToolbar")
@@ -10,28 +17,35 @@ class FileToolbar(QToolBar):
         self.setOrientation(Qt.Orientation.Horizontal)
         self.setFloatable(False)
         self.setMovable(False)
+        self.tooltips = self.load_tooltips(self.tooltips_path)
         self.addWidget(self.create_file_widget())
         self.addSeparator()
         self.addWidget(self.create_export_widget())
         self.addSeparator()
         self.addWidget(self.create_print_widget())
         self.addSeparator()
-        self.addWidget(self.create_find_widget())
+        self.addWidget(self.create_search_widget())
 
     def create_file_widget(self) -> QWidget:
         file_widget = QWidget()
         file_layout = QHBoxLayout()
         new_file_button = QPushButton()
+        self.set_icon(new_file_button, "new_file_icon.png")
+        self.set_tooltips(new_file_button, "new_file")
         open_file_button = QPushButton()
+        self.set_icon(open_file_button, "open_file_icon.png")
+        self.set_tooltips(open_file_button, "open_file")
         save_as_button = QPushButton()
+        self.set_icon(save_as_button, "save_as_icon.png")
+        self.set_tooltips(save_as_button, "save_as_file")
         self.save_button = QPushButton()
         self.save_button.setDisabled(True)
-        close_button = QPushButton()
+        self.set_icon(self.save_button, "save_icon.png")
+        self.set_tooltips(self.save_button, "save_file")
         file_layout.addWidget(new_file_button)
         file_layout.addWidget(open_file_button)
         file_layout.addWidget(save_as_button)
         file_layout.addWidget(self.save_button)
-        file_layout.addWidget(close_button)
         file_widget.setLayout(file_layout)
         return file_widget
 
@@ -39,7 +53,11 @@ class FileToolbar(QToolBar):
         export_widget = QWidget()
         export_layout = QHBoxLayout()
         save_as_html_button = QPushButton()
+        self.set_icon(save_as_html_button, "save_as_html_icon.png")
+        self.set_tooltips(save_as_html_button, "save_as_html")
         export_pdf_button = QPushButton()
+        self.set_icon(export_pdf_button, "export_pdf_icon.png")
+        self.set_tooltips(export_pdf_button, "export_to_pdf")
         export_layout.addWidget(save_as_html_button)
         export_layout.addWidget(export_pdf_button)
         export_widget.setLayout(export_layout)
@@ -49,21 +67,43 @@ class FileToolbar(QToolBar):
         print_widget = QWidget()
         print_layout = QHBoxLayout()
         print_preview_button = QPushButton()
+        self.set_icon(print_preview_button, "print_preview_icon.png")
+        self.set_tooltips(print_preview_button, "print_preview")
         print_button = QPushButton()
+        self.set_icon(print_button, "print_icon.png")
+        self.set_tooltips(print_button, "print_document")
         print_layout.addWidget(print_preview_button)
         print_layout.addWidget(print_button)
         print_widget.setLayout(print_layout)
         return print_widget
 
-    def create_find_widget(self) -> QWidget:
-        find_widget = QWidget()
-        find_layout = QHBoxLayout()
-        find_label = QLabel("Find:")
-        self.find_input = QLineEdit()
-        find_button = QPushButton()
-        find_layout.addWidget(find_label)
-        find_layout.addWidget(self.find_input)
-        find_layout.addWidget(find_button)
-        find_layout.addStretch()
-        find_widget.setLayout(find_layout)
-        return find_widget
+    def create_search_widget(self) -> QWidget:
+        search_widget = QWidget()
+        search_layout = QHBoxLayout()
+        search_label = QLabel("Search:")
+        self.search_input = QLineEdit()
+        self.search_input.setPlaceholderText("for search write here...")
+        search_button = QPushButton()
+        self.set_icon(search_button, "search_icon.png")
+        self.set_tooltips(search_button, "search")
+        search_layout.addWidget(search_label)
+        search_layout.addWidget(self.search_input)
+        search_layout.addWidget(search_button)
+        search_layout.addStretch()
+        search_widget.setLayout(search_layout)
+        return search_widget
+
+    def set_icon(self, widget: QPushButton, icon_name: str) -> None:
+        widget.setIcon(QIcon(str(self.icons_path.joinpath(icon_name))))
+        widget.setIconSize(QSize(20, 20))
+
+    @staticmethod
+    def load_tooltips(tooltips_path: str) -> dict:
+        with open(tooltips_path, "r", encoding="utf-8") as file:
+            tooltips = json.load(file)
+            return tooltips["file_tooltips"]
+
+    def set_tooltips(self, widget: QWidget, tooltip_key: str) -> None:
+        tooltip_text = self.tooltips.get(tooltip_key)
+        widget.setToolTip(tooltip_text)
+        widget.setToolTipDuration(5000)
--- src/ui/widgets/text_toolbar.py
+++ src/ui/widgets/text_toolbar.py
@@ -1,8 +1,18 @@
-from PyQt6.QtCore import Qt
+import json
+import pathlib
+
+from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtGui import QIcon, QColor, QPixmap, QFont
 from PyQt6.QtWidgets import QToolBar, QWidget, QHBoxLayout, QComboBox, QPushButton
 
 
 class TextToolbar(QToolBar):
+    tooltips_path = str(pathlib.Path(__file__).parent.parent.joinpath("tooltips", "tooltips_en.json"))
+    icons_path = pathlib.Path(__file__).parent.parent.joinpath("icons", "text_icons")
+    font_list = ["Arial", "Calibri", "Comic Sans MS", "Courier New", "Georgia", "Helvetica", "Palatino", "Tahoma", "Times New Roman", "Verdana"]
+    font_sizes = [8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+    combobox_colors = ["#000000", "#FFFFFF", "#1E90FF", "#32CD32", "#FF4500", "#FFA500", "#FFFF00", "#9370DB", "#8B4513", "#B22222"]
+
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.setObjectName("textToolbar")
@@ -10,6 +20,7 @@ class TextToolbar(QToolBar):
         self.setOrientation(Qt.Orientation.Horizontal)
         self.setFloatable(False)
         self.setMovable(False)
+        self.tooltips = self.load_tooltips(self.tooltips_path)
         self.addWidget(self.create_font_widget())
         self.addSeparator()
         self.addWidget(self.create_style_widget())
@@ -24,7 +35,18 @@ class TextToolbar(QToolBar):
         font_widget = QWidget()
         font_layout = QHBoxLayout()
         self.font_family_combobox = QComboBox()
+        self.font_family_combobox.setObjectName("fontFamily")
+        for font in self.font_list:
+            self.font_family_combobox.addItem(font)
+            index = self.font_family_combobox.count() - 1
+            self.font_family_combobox.setItemData(index, QFont(font), role=Qt.ItemDataRole.FontRole)
+        self.set_tooltips(self.font_family_combobox, "font")
         self.font_size_combobox = QComboBox()
+        self.font_size_combobox.setObjectName("fontSize")
+        for size in self.font_sizes:
+            self.font_size_combobox.addItem(str(size))
+        self.font_size_combobox.setCurrentText("14")
+        self.set_tooltips(self.font_size_combobox, "font_size")
         font_layout.addWidget(self.font_family_combobox)
         font_layout.addWidget(self.font_size_combobox)
         font_widget.setLayout(font_layout)
@@ -34,9 +56,17 @@ class TextToolbar(QToolBar):
         style_widget = QWidget()
         style_layout = QHBoxLayout()
         bold_text_button = QPushButton()
+        self.set_icons(bold_text_button, "bold_icon.png")
+        self.set_tooltips(bold_text_button, "bold_text")
         italic_text_button = QPushButton()
+        self.set_icons(italic_text_button, "italic_icon.png")
+        self.set_tooltips(italic_text_button, "italic_text")
         underline_text_button = QPushButton()
+        self.set_icons(underline_text_button, "underline_icon.png")
+        self.set_tooltips(underline_text_button, "underline_text")
         strikethrough_text_button = QPushButton()
+        self.set_icons(strikethrough_text_button, "strikethrough_icon.png")
+        self.set_tooltips(strikethrough_text_button, "strikethrough_text")
         style_layout.addWidget(bold_text_button)
         style_layout.addWidget(italic_text_button)
         style_layout.addWidget(underline_text_button)
@@ -48,19 +78,33 @@ class TextToolbar(QToolBar):
         action_widget = QWidget()
         action_layout = QHBoxLayout()
         self.undo_button = QPushButton()
+        self.set_icons(self.undo_button, "undo_icon.png")
+        self.set_tooltips(self.undo_button, "undo")
         self.redo_button = QPushButton()
-        self.copy_button = QPushButton()
-        self.paste_button = QPushButton()
+        self.set_icons(self.redo_button, "redo_icon.png")
+        self.set_tooltips(self.redo_button, "redo")
         self.cut_button = QPushButton()
         select_all_button = QPushButton()
-        delete_all_button = QPushButton()
+        self.set_icons(self.cut_button, "cut_icon.png")
+        self.set_tooltips(self.cut_button, "cut")
+        self.copy_button = QPushButton()
+        self.set_icons(self.copy_button, "copy_icon.png")
+        self.set_tooltips(self.copy_button, "copy")
+        self.paste_button = QPushButton()
+        self.set_icons(self.paste_button, "paste_icon.png")
+        self.set_tooltips(self.paste_button, "paste")
+        self.set_icons(select_all_button, "select_icon.png")
+        self.set_tooltips(select_all_button, "select_all")
+        delete_text_button = QPushButton()
+        self.set_icons(delete_text_button, "delete_icon")
+        self.set_tooltips(delete_text_button, "delete_text")
         action_layout.addWidget(self.undo_button)
         action_layout.addWidget(self.redo_button)
+        action_layout.addWidget(self.cut_button)
         action_layout.addWidget(self.copy_button)
         action_layout.addWidget(self.paste_button)
-        action_layout.addWidget(self.cut_button)
         action_layout.addWidget(select_all_button)
-        action_layout.addWidget(delete_all_button)
+        action_layout.addWidget(delete_text_button)
         action_widget.setLayout(action_layout)
         return action_widget
 
@@ -68,9 +112,17 @@ class TextToolbar(QToolBar):
         align_widget = QWidget()
         align_layout = QHBoxLayout()
         align_left_button = QPushButton()
+        self.set_icons(align_left_button, "align_left_icon.png")
+        self.set_tooltips(align_left_button, "align_left")
         align_center_button = QPushButton()
+        self.set_icons(align_center_button, "align_center_icon.png")
+        self.set_tooltips(align_center_button, "align_center")
         align_right_button = QPushButton()
+        self.set_icons(align_right_button, "align_right_icon.png")
+        self.set_tooltips(align_right_button, "align_right")
         align_justified_button = QPushButton()
+        self.set_icons(align_justified_button, "align_justify_icon.png")
+        self.set_tooltips(align_justified_button, "align_justified")
         align_layout.addWidget(align_left_button)
         align_layout.addWidget(align_center_button)
         align_layout.addWidget(align_right_button)
@@ -82,8 +134,44 @@ class TextToolbar(QToolBar):
         colors_widget = QWidget()
         colors_layout = QHBoxLayout()
         self.text_color_combobox = QComboBox()
+        self.text_color_combobox.setObjectName("textColor")
+        for color in self.combobox_colors:
+            icon = self.create_color_icon(color)
+            self.text_color_combobox.addItem(icon, "")
+        default_color = "#000000"
+        default_index = self.combobox_colors.index(default_color)
+        self.text_color_combobox.setCurrentIndex(default_index)
+        self.set_tooltips(self.text_color_combobox, "text_color")
         self.background_color_combobox = QComboBox()
+        self.background_color_combobox.setObjectName("backgroundColor")
+        for color in self.combobox_colors:
+            icon = self.create_color_icon(color)
+            self.background_color_combobox.addItem(icon, "")
+        default_color = "#FFFFFF"
+        default_index = self.combobox_colors.index(default_color)
+        self.background_color_combobox.setCurrentIndex(default_index)
+        self.set_tooltips(self.background_color_combobox, "background_color")
         colors_layout.addWidget(self.text_color_combobox)
         colors_layout.addWidget(self.background_color_combobox)
         colors_widget.setLayout(colors_layout)
         return colors_widget
+
+    def set_icons(self, widget: QPushButton, icon_name: str) -> None:
+        widget.setIcon(QIcon(str(self.icons_path.joinpath(str(icon_name)))))
+        widget.setIconSize(QSize(20, 20))
+
+    @staticmethod
+    def load_tooltips(tooltips_path: str) -> dict:
+        with open(tooltips_path, "r", encoding="utf-8") as file:
+            tooltips = json.load(file)
+            return tooltips["text_tooltips"]
+
+    def set_tooltips(self, widget: QWidget, tooltip_key: str) -> None:
+        tooltip_text = self.tooltips.get(tooltip_key)
+        widget.setToolTip(tooltip_text)
+        widget.setToolTipDuration(5000)
+
+    def create_color_icon(self, color_hex):
+        pixmap = QPixmap(30, 30)
+        pixmap.fill(QColor(color_hex))
+        return QIcon(pixmap)

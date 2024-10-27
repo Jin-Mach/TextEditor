5067449483565da1ba75fc9a2830f725c834217d
Jin-Mach <Ji82Ma@seznam.cz>
2024-10-27 14:35:24 +0100

    Added error message handling for loading the styles.qss file. Added logging to the .log file when the styles file fails to load.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,41 @@
 import pathlib
 import sys
 
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QMessageBox
 
 from src.ui.main_window import MainWindow
+from src.utilities.logging_manager import setup_logger
 
 def create_app():
     application = QApplication(sys.argv)
+
+    style_file_path = pathlib.Path(__file__).parent.joinpath("style", "styles.qss")
     try:
-        with open(str(pathlib.Path(__file__).parent.joinpath("style", "styles.qss")), "r") as file:
+        with open(style_file_path, "r") as file:
             application.setStyleSheet(file.read())
     except Exception as e:
-        print(f"Error: {e}")
+        setup_logger().error(str(e))
+
+        message_box = QMessageBox()
+        message_box.setWindowTitle("Error Loading Styles")
+        message_box.setIcon(QMessageBox.Icon.Critical)
+        message_box.setText(
+            f"Unable to load 'styles.qss'.\n"
+            f"Some features (like print preview, printing) may not work correctly.\n"
+            f"Please check the file location: {style_file_path}")
+
+        continue_button = message_box.addButton("Continue", QMessageBox.ButtonRole.AcceptRole)
+        continue_button.setToolTip("Opens the editor without styles.")
+        continue_button.setToolTipDuration(5000)
+        cancel_button = message_box.addButton("Cancel", QMessageBox.ButtonRole.RejectRole)
+        cancel_button.setToolTip("Closes the dialog and exits the application.")
+        cancel_button.setToolTipDuration(5000)
+
+        message_box.exec()
+
+        if message_box.clickedButton() == cancel_button:
+            sys.exit(1)
+
     window = MainWindow()
     window.show()
     sys.exit(application.exec())
---
+++ src/utilities/logging_manager.py
@@ -0,0 +1,17 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import pathlib
+
+logging_file = pathlib.Path.joinpath(pathlib.Path(__file__).parent.parent, "data", "log_files", "texteditor_errors.log")
+
+def setup_logger() -> logging.Logger:
+    formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s() - %(message)s',
+                                 datefmt='%Y-%m-%d %H:%M:%S')
+    handler = RotatingFileHandler(logging_file, mode="a", encoding="utf-8", maxBytes=5 * 1024 * 1024, backupCount=1)
+    handler.setFormatter(formater)
+    logger = logging.getLogger(pathlib.Path(__name__).name)
+    logger.setLevel(logging.WARNING)
+
+    if not logger.hasHandlers():
+        logger.addHandler(handler)
+    return logger

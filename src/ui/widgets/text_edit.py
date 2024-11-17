7913c54683d647fffb0a8bbf9f4e7c9c98b645bf
Jin-Mach <Ji82Ma@seznam.cz>
2024-11-17 10:19:07 +0100

    In the TextEdit class, a function update_context_actions was created, which sets the enabled state for context items such as copy, paste, undo, and redo. In the TextToolbar class, a function update_edit_buttons was created, which does the same for the buttons, and a function update_format_buttons that uses the new get_text_format function (returns the current document settings such as font, size, alignment, bold, etc.). Two static functions block_signals and activate_signals were added to set toolbar buttons to checked/unchecked states. Minor code adjustments.

--- src/ui/widgets/text_edit.py
+++ src/ui/widgets/text_edit.py
@@ -1,4 +1,4 @@
-from PyQt6.QtGui import QTextOption, QAction, QFont
+from PyQt6.QtGui import QTextOption, QAction, QFont, QGuiApplication
 from PyQt6.QtWidgets import QTextEdit, QStatusBar, QMenu, QToolBar
 
 from src.utilities.data_provider import DataProvider
@@ -15,7 +15,9 @@ class TextEdit(QTextEdit):
         self.setTabStopDistance(50)
         self.setAcceptRichText(True)
         self.set_ui_text()
+        self.update_context_actions()
         self.cursorPositionChanged.connect(self.refresh_count_labels)
+        self.cursorPositionChanged.connect(self.update_context_actions)
 
     def refresh_count_labels(self) -> None:
         lines_count = self.document().blockCount()
@@ -39,12 +41,19 @@ class TextEdit(QTextEdit):
     def set_ui_text(self) -> None:
         ui_text = DataProvider.get_ui_text("textedit")
         self.undo_action = QAction(ui_text.get("undoContext"), self)
+        self.undo_action.triggered.connect(self.undo)
         self.redo_action = QAction(ui_text.get("redoContext"), self)
+        self.redo_action.triggered.connect(self.redo)
         self.cut_action = QAction(ui_text.get("cutContext"), self)
+        self.cut_action.triggered.connect(self.cut)
         self.copy_action = QAction(ui_text.get("copyContext"), self)
+        self.copy_action.triggered.connect(self.copy)
         self.paste_action = QAction(ui_text.get("pasteContext"), self)
+        self.paste_action.triggered.connect(self.paste)
         self.select_action = QAction(ui_text.get("selectContext"), self)
+        self.select_action.triggered.connect(self.selectAll)
         self.delete_action = QAction(ui_text.get("deleteContext"), self)
+        self.delete_action.triggered.connect(self.clear)
 
     def reset_text_edit(self) -> None:
         self.clear()
@@ -66,8 +75,19 @@ class TextEdit(QTextEdit):
     def get_text_format(self) -> tuple:
         cursor = self.textCursor()
         char_format = cursor.charFormat()
-        is_bold = char_format.fontWeight() == QFont.Weight.Bold
-        is_italic = char_format.fontItalic()
-        is_underline = char_format.fontUnderline()
-        is_strikeout = char_format.fontStrikeOut()
-        return is_bold, is_italic, is_underline, is_strikeout
+        if cursor and char_format.isValid():
+            is_bold = char_format.fontWeight() == QFont.Weight.Bold
+            is_italic = char_format.fontItalic()
+            is_underline = char_format.fontUnderline()
+            is_strikeout = char_format.fontStrikeOut()
+            return is_bold, is_italic, is_underline, is_strikeout
+        return False, False, False, False
+
+    def update_context_actions(self) -> None:
+        self.undo_action.setEnabled(self.document().isUndoAvailable())
+        self.redo_action.setEnabled(self.document().isRedoAvailable())
+        self.cut_action.setEnabled(self.textCursor().hasSelection())
+        self.copy_action.setEnabled(self.textCursor().hasSelection())
+        self.paste_action.setEnabled(bool(QGuiApplication.clipboard().text()))
+        self.select_action.setEnabled(bool(self.toPlainText()))
+        self.delete_action.setEnabled(bool(self.toPlainText()))
--- src/ui/widgets/text_toolbar.py
+++ src/ui/widgets/text_toolbar.py
@@ -1,7 +1,7 @@
 import pathlib
 
 from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtGui import QIcon, QColor, QPixmap, QFont
+from PyQt6.QtGui import QIcon, QColor, QPixmap, QFont, QGuiApplication
 from PyQt6.QtWidgets import QToolBar, QWidget, QHBoxLayout, QComboBox, QPushButton
 
 from src.ui.widgets.text_edit import TextEdit
@@ -11,7 +11,7 @@ from src.utilities.text_manager import TextManager
 
 # noinspection PyUnresolvedReferences
 class TextToolbar(QToolBar):
-    font_list = ["Arial", "Calibri", "Comic Sans MS", "Courier New", "Georgia", "Helvetica", "Palatino", "Tahoma", "Times New Roman", "Verdana"]
+    font_list = ["Arial", "Calibri", "Comic Sans MS", "Courier New", "Georgia", "Impact", "Tahoma", "Times New Roman", "Trebuchet MS", "Verdana"]
     font_sizes = [8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
     combobox_colors = ["#000000", "#FFFFFF", "#1E90FF", "#32CD32", "#FF4500", "#FFA500", "#FFFF00", "#9370DB", "#8B4513", "#B22222"]
 
@@ -28,6 +28,9 @@ class TextToolbar(QToolBar):
         self.set_icons()
         self.set_tooltips()
         self.create_connection()
+        self.update_edit_buttons()
+        self.text_edit.cursorPositionChanged.connect(self.update_edit_buttons)
+        self.text_edit.cursorPositionChanged.connect(self.update_format_buttons)
 
     def create_main_gui(self) -> None:
         self.addWidget(self.create_font_widget())
@@ -64,16 +67,20 @@ class TextToolbar(QToolBar):
         style_layout = QHBoxLayout()
         self.bold_text_button = QPushButton()
         self.bold_text_button.setObjectName("bold")
+        self.bold_text_button.setCheckable(True)
         self.italic_text_button = QPushButton()
         self.italic_text_button.setObjectName("italic")
+        self.italic_text_button.setCheckable(True)
         self.underline_text_button = QPushButton()
         self.underline_text_button.setObjectName("underline")
-        self.strikethrough_text_button = QPushButton()
-        self.strikethrough_text_button.setObjectName("strikethrough")
+        self.underline_text_button.setCheckable(True)
+        self.strikeout_text_button = QPushButton()
+        self.strikeout_text_button.setObjectName("strikeout")
+        self.strikeout_text_button.setCheckable(True)
         style_layout.addWidget(self.bold_text_button)
         style_layout.addWidget(self.italic_text_button)
         style_layout.addWidget(self.underline_text_button)
-        style_layout.addWidget(self.strikethrough_text_button)
+        style_layout.addWidget(self.strikeout_text_button)
         style_widget.setLayout(style_layout)
         return style_widget
 
@@ -82,18 +89,25 @@ class TextToolbar(QToolBar):
         action_layout = QHBoxLayout()
         self.undo_button = QPushButton()
         self.undo_button.setObjectName("undo")
+        self.undo_button.clicked.connect(self.text_edit.undo)
         self.redo_button = QPushButton()
         self.redo_button.setObjectName("redo")
+        self.redo_button.clicked.connect(self.text_edit.redo)
         self.cut_button = QPushButton()
         self.cut_button.setObjectName("cut")
+        self.cut_button.clicked.connect(self.text_edit.cut)
         self.copy_button = QPushButton()
         self.copy_button.setObjectName("copy")
+        self.copy_button.clicked.connect(self.text_edit.copy)
         self.paste_button = QPushButton()
         self.paste_button.setObjectName("paste")
+        self.paste_button.clicked.connect(self.text_edit.paste)
         self.select_all_button = QPushButton()
         self.select_all_button.setObjectName("selectAll")
+        self.select_all_button.clicked.connect(self.text_edit.selectAll)
         self.delete_text_button = QPushButton()
         self.delete_text_button.setObjectName("deleteText")
+        self.delete_text_button.clicked.connect(self.text_edit.clear)
         action_layout.addWidget(self.undo_button)
         action_layout.addWidget(self.redo_button)
         action_layout.addWidget(self.cut_button)
@@ -109,12 +123,20 @@ class TextToolbar(QToolBar):
         align_layout = QHBoxLayout()
         self.align_left_button = QPushButton()
         self.align_left_button.setObjectName("alignLeft")
+        self.align_left_button.setCheckable(True)
+        self.align_left_button.clicked.connect(lambda: self.text_edit.setAlignment(Qt.AlignmentFlag.AlignLeft))
         self.align_center_button = QPushButton()
         self.align_center_button.setObjectName("alignCenter")
+        self.align_center_button.setCheckable(True)
+        self.align_center_button.clicked.connect(lambda: self.text_edit.setAlignment(Qt.AlignmentFlag.AlignCenter))
         self.align_right_button = QPushButton()
         self.align_right_button.setObjectName("alignRight")
+        self.align_right_button.setCheckable(True)
+        self.align_right_button.clicked.connect(lambda: self.text_edit.setAlignment(Qt.AlignmentFlag.AlignRight))
         self.align_justified_button = QPushButton()
-        self.align_justified_button.setObjectName("alignJustified")
+        self.align_justified_button.setObjectName("alignJustify")
+        self.align_justified_button.setCheckable(True)
+        self.align_justified_button.clicked.connect(lambda: self.text_edit.setAlignment(Qt.AlignmentFlag.AlignJustify))
         align_layout.addWidget(self.align_left_button)
         align_layout.addWidget(self.align_center_button)
         align_layout.addWidget(self.align_right_button)
@@ -180,8 +202,67 @@ class TextToolbar(QToolBar):
         self.bold_text_button.clicked.connect(lambda: self.text_manager.set_text_format("bold"))
         self.italic_text_button.clicked.connect(lambda: self.text_manager.set_text_format("italic"))
         self.underline_text_button.clicked.connect(lambda: self.text_manager.set_text_format("underline"))
-        self.strikethrough_text_button.clicked.connect(lambda: self.text_manager.set_text_format("strikeout"))
+        self.strikeout_text_button.clicked.connect(lambda: self.text_manager.set_text_format("strikeout"))
 
     def reset_text_toolbar(self) -> None:
         self.font_family_combobox.setCurrentText("Arial")
         self.font_size_combobox.setCurrentText("14")
+
+    def update_edit_buttons(self) -> None:
+        self.undo_button.setEnabled(self.text_edit.document().isUndoAvailable())
+        self.redo_button.setEnabled(self.text_edit.document().isRedoAvailable())
+        self.cut_button.setEnabled(self.text_edit.textCursor().hasSelection())
+        self.copy_button.setEnabled(self.text_edit.textCursor().hasSelection())
+        self.paste_button.setEnabled(bool(QGuiApplication.clipboard().text()))
+        self.select_all_button.setEnabled(bool(self.text_edit.toPlainText()))
+        self.delete_text_button.setEnabled(bool(self.text_edit.toPlainText()))
+
+    def update_format_buttons(self) -> None:
+        text_format = self.get_text_format()
+        alignment_buttons = [self.align_left_button, self.align_center_button, self.align_right_button, self.align_justified_button]
+        for alignmentbutton in alignment_buttons:
+            alignmentbutton.setChecked(False)
+        widgets = [self.font_family_combobox, self.font_size_combobox, self.bold_text_button, self.italic_text_button,
+                   self.underline_text_button, self.strikeout_text_button, self.findChild(QPushButton, text_format[6])]
+        alignment_button = widgets[-1]
+        self.block_signals(widgets)
+        self.font_family_combobox.setCurrentText(text_format[0])
+        self.font_size_combobox.setCurrentText(text_format[1])
+        self.bold_text_button.setChecked(text_format[2])
+        self.italic_text_button.setChecked(text_format[3])
+        self.underline_text_button.setChecked(text_format[4])
+        self.strikeout_text_button.setChecked(text_format[5])
+        alignment_button.setChecked(True)
+        self.activate_signals(widgets)
+
+    def get_text_format(self) -> tuple:
+        cursor = self.text_edit.textCursor()
+        char_format = cursor.charFormat()
+        alignment = self.text_edit.alignment()
+        alignment_name = "alignLeft"
+        if cursor and char_format.isValid():
+            font = char_format.font()
+            font_family = font.family()
+            if not font_family:
+                font_family = "Arial"
+            font_size = font.pointSize()
+            if not font_size:
+                font_size = 14
+            is_bold = font.bold()
+            is_italic = font.italic()
+            is_underline = font.underline()
+            is_strikeout = font.strikeOut()
+            if alignment:
+                alignment_name = alignment.name[0].lower() + alignment.name[1:]
+            return str(font_family), str(font_size), is_bold, is_italic, is_underline, is_strikeout, alignment_name
+        return "Arial", "14", False, False, False, False, "alignLeft"
+
+    @staticmethod
+    def block_signals(widgets: list) -> None:
+        for widget in widgets:
+            widget.blockSignals(True)
+
+    @staticmethod
+    def activate_signals(widgets: list) -> None:
+        for widget in widgets:
+            widget.blockSignals(False)
